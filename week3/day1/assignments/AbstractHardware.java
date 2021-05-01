bae38d7fc6b11bbe48f355bb06bcc9dcf5833737
Durga1030 <82827475+Durga1030@users.noreply.github.com>
2021-05-02 02:14:55 +0530

    Delete week3/day1 directory

--- week3/day1/assignments/AbstractHardware.java
+++
@@ -1,9 +0,0 @@
-package week3.day1.assignments;
-
-public interface AbstractHardware {
-
-	public static void hardwareResources() {
-		System.out.println("Hardware resources");
-
-	}
-}
--- week3/day1/assignments/AbstractSoftware.java
+++
@@ -1,6 +0,0 @@
-package week3.day1.assignments;
-
-public interface AbstractSoftware {
-
-	public boolean softwareResources();
-}
