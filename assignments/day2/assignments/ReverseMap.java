6ecf790506d3811c17158f3580c749e741f7f83f
Durga1030 <82827475+Durga1030@users.noreply.github.com>
2021-05-06 19:20:42 +0530

    Add files via upload

---
+++ assignments/day2/assignments/ReverseMap.java
@@ -0,0 +1,36 @@
+package week3.day2.assignments;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class ReverseMap {
+	public static void main(String[] args) {
+
+//	Insert elements to the map
+		/*
+		 * Input: - Employee Info: Emp_id|Emp_name 100 Hari 101 Naveen 102 Sam 104
+		 * Balaji
+		 */
+
+		TreeMap<Integer, String> map = new TreeMap<>(Collections.reverseOrder());
+		map.put(100, "Hari");
+		map.put(101, "Naveen");
+		map.put(102, "Sam");
+		map.put(104, "Balaji");
+
+		Set set = map.entrySet();
+		Iterator i = set.iterator();
+
+		while (i.hasNext()) {
+
+			Map.Entry mapEntry = (Map.Entry) i.next();
+			System.out.print(mapEntry.getKey() + ": ");
+			System.out.println(mapEntry.getValue());
+
+		}
+
+	}
+}
